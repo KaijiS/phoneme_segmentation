ae17d8e25910fd8240a9680a97252273f47dbbdd
KaijiS <31200760+KaijiS@users.noreply.github.com>
2020-11-11 20:36:50 +0900

    feat: サンプルデータとバリデーション処理

--- app/controllers/phoneme_segmentation.py
+++ app/controllers/phoneme_segmentation.py
@@ -4,22 +4,22 @@ from typing import List
 from schemas.OriginalVoiceWaveform import OriginalVoiceWaveform
 from schemas.Phoneme import Phoneme
 
-from services.phoneme_segmentation_service import PhonemeSegmentationService
+from services import phoneme_segmentation_service
 
 router = APIRouter()
 
 @router.get('/')
-def connection_check() -> dict[str: str]:
+async def connection_check() -> dict[str: str]:
   '''
   意思疎通確認用API
   '''
   return {'接続':'確認！'}
 
 
-@router.post('/', response_model=List[Phoneme])
-def phoneme_segmentation(originalVoiceWaveform: OriginalVoiceWaveform) -> List[Phoneme]:
+@router.post('/', response_model=List[Phoneme], status_code=201)
+async def phoneme_segmentation(originalVoiceWaveform: OriginalVoiceWaveform) -> List[Phoneme]:
   '''
   音素セグメンテーションの実行API
   '''
 
-  return PhonemeSegmentationService.phoneme_segmentation(originalVoiceWaveform)
+  return phoneme_segmentation_service.phoneme_segmentation(originalVoiceWaveform)
--- app/services/phoneme_segmentation_service.py
+++ app/services/phoneme_segmentation_service.py
@@ -2,41 +2,55 @@ from schemas.OriginalVoiceWaveform import OriginalVoiceWaveform
 from schemas.Phoneme import Phoneme
 from typing import List
 
-class PhonemeSegmentationService:
+import base64
 
-  @staticmethod
-  def phoneme_segmentation(originalVoiceWaveform: OriginalVoiceWaveform) -> List[Phoneme]:
-    ''' 音素セグメンテーションを実行
+from components.WaveData import WaveData
 
-    Parameters
-    --------------
-    originalVoiceWaveform: OriginalVoiceWaveform
+def phoneme_segmentation(originalVoiceWaveform: OriginalVoiceWaveform) -> List[Phoneme]:
+  ''' 音素セグメンテーションを実行
 
-    Returns
-    --------------
-    List[Phoneme]
-    '''
+  Parameters
+  --------------
+  originalVoiceWaveform: OriginalVoiceWaveform
 
-    # TODO 音声ファイルの中身確認
+  Returns
+  --------------
+  List[Phoneme]
+  '''
 
-    # TODO 音声ファイルとテキストファイル保存
+  # TODO 音声ファイルの中身確認
+  check_wavefile(originalVoiceWaveform.wavedata_base64)
 
-    # TODO perl実行(kitをgit cloneしてくる)
+  # TODO 音声ファイルとテキストファイル保存
 
-    # TODO 終了 or どこかで失敗した場合保存したファイルすべて削除(finally句を使用)
+  # TODO perl実行(kitをgit cloneしてくる)
 
-    # TODO 型にはめてレスポンス
+  # TODO 終了 or どこかで失敗した場合保存したファイルすべて削除(finally句を使用)
 
-    phonemes: List[Phoneme] =  []
-    for i in range(2):
-      phoneme: Phoneme = Phoneme(
-        phoneme = 'a',
-        wavedata = 'wavedata_base64',
-        filename = 'filename_a_00001111000.wav',
-        array_index_from = 20,
-        array_index_to = 39,
-        n_score=0.987
-      )
-      phonemes.append(phoneme)
+  # TODO 型にはめてレスポンス
 
-    return phonemes
+  phonemes: List[Phoneme] =  []
+  for i in range(2):
+    phoneme: Phoneme = Phoneme(
+      phoneme = 'a',
+      wavedata = 'wavedata_base64',
+      filename = 'filename_a_00001111000.wav',
+      array_index_from = 20,
+      array_index_to = 39,
+      n_score=0.987
+    )
+    phonemes.append(phoneme)
+
+  return phonemes
+
+
+
+def check_wavefile(wavedata_base64: str):
+
+  # まずバイナリに変更
+  binary: bytes = base64.b64decode(wavedata_base64.encode("UTF-8"))
+  wavedata: WaveData = WaveData(binary)
+  print(wavedata.riff)
+  print(wavedata.format)
+  wavedata.validate()
+  return
