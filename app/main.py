5d47406868fb9cc52aa96c7f07613f7ab2a3c4c2
KaijiS <31200760+KaijiS@users.noreply.github.com>
2020-11-11 23:41:20 +0900

    feat: サンプルでの確認OK

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from controllers import phoneme_segmentation
 app = FastAPI(
   title='音素セグメンテーションAPI',
   description='Juliusを用いた音素ラベリングセグメンテーションの実行',
-  version='0.1 beta'
+  version='0.1'
 )
 
 # ルーティングをinclude
