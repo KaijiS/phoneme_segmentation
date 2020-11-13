ee7b9f5749b161b585e116115f57aa78671abf38
KaijiS <31200760+KaijiS@users.noreply.github.com>
2020-11-14 01:24:33 +0900

    feat: レスポンスのパフォーマンスを高くするためorjsonを使用

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from controllers import phoneme_segmentation
+from fastapi.responses import ORJSONResponse
 
 app = FastAPI(
   title='音素セグメンテーションAPI',
   description='Juliusを用いた音素ラベリングセグメンテーションの実行',
-  version='0.1'
+  version='0.1',
+  # デフォルトの応答クラスを指定: ORJSONResponseｰ>パフォーマンス高い
+  default_response_class=ORJSONResponse
 )
 
 # ルーティングをinclude
